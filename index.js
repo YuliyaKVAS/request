cef38d184e0aaa719151a71a95cba81f441f7aa7
YuliyaKVAS <yuliya2095@bk.ru>
2018-10-24 23:32:44 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var url = new URL('https://api.nytimes.com/svc/books/v3/lists/overview.json');
+    var user_args = {
+                     'published_date' : "2011-05-19", 
+                     'api-key': "616db80ea28941bf9ebe50fb3963ee62"
+                    };
+    url.search = new URLSearchParams(user_args)
+    fetch(url, {method:"GET"})
+    .then((request) => request.json())
+    .then((data) => data.results.lists.map(function(element){
+        return (element.display_name + ' --- ' + element.books.length);
+    }))
+    .then(array => array.forEach(value => console.log(value)));
