323fcb86d6d1b407faf6f0d3b9fbdf4902593b83
YuliyaKVAS <yuliya2095@bk.ru>
2018-10-25 09:13:34 +0300

    files is changed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var url = new URL('https://api.nytimes.com/svc/books/v3/lists/overview.json');
-    var user_args = {
+    var user_param = {
                      'published_date' : "2011-05-19", 
                      'api-key': "616db80ea28941bf9ebe50fb3963ee62"
                     };
-    url.search = new URLSearchParams(user_args)
+    url.search = new URLSearchParams(user_param); //URL is constructed taking into account the request parametrs
     fetch(url, {method:"GET"})
     .then((request) => request.json())
     .then((data) => data.results.lists.map(function(element){
